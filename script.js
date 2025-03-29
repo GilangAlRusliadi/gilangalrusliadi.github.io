5a5a8f4ea27c49a6247ad47da4002ba371038244
GilangAlRusliadi <gilangxavier@gmail.com>
2025-03-29 22:16:36 +0700

    Script

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let frame = document.getElementById("dynamicFrame");
+    let overlay = document.querySelector(".overlay");
+    let urls = [
+        "https://gilbertclaus.pythonanywhere.com/animated/orbit",
+        "https://gilbertclaus.pythonanywhere.com/animated/star",
+        "https://gilangalrusliadi.github.io/Login/",
+        "https://gilbertclaus.pythonanywhere.com/animated/star2"
+    ];
+    let index = 0;
+
+    setInterval(() => {
+        // Mulai fade-in overlay hitam
+        overlay.style.opacity = "1";
+
+        setTimeout(() => {
+            // Ganti URL setelah fade-out selesai
+            index = (index + 1) % urls.length;
+            frame.src = urls[index];
+
+            // Setelah sedikit delay, fade-out overlay hitam
+            setTimeout(() => {
+                overlay.style.opacity = "0";
+            }, 500);
+        }, 500); // Waktu overlay hitam sebelum mengganti src
+    }, 10000);
+});
