9090bf3786ba32ee3bf91cc68573ca7aca559990
GilangAlRusliadi <gilangxavier@gmail.com>
2025-03-31 16:02:27 +0700

    Orbit

--- script.js
+++ script.js
@@ -2,12 +2,13 @@ document.addEventListener("DOMContentLoaded", function () {
     let frame = document.getElementById("dynamicFrame");
     let overlay = document.querySelector(".overlay");
     let urls = [
-        "https://gilbertclaus.pythonanywhere.com/animated/star",
         "https://gilbertclaus.pythonanywhere.com/animated/orbit",
+        "https://gilbertclaus.pythonanywhere.com/animated/star",
         "https://gilbertclaus.pythonanywhere.com/animated/star2"
         // "https://gilangalrusliadi.github.io/Login/",
     ];
     let index = 0;
+    frame.src = urls[0];
 
     setInterval(() => {
         // Mulai fade-in overlay hitam
