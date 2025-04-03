793b3dc3778c4d70b0d423239256694ba4ed2a93
GilangAlRusliadi <gilangxavier@gmail.com>
2025-04-03 19:03:56 +0700

    Text

--- static/script.js
+++ static/script.js
@@ -1,33 +1,33 @@
 const iconsData = [
     {
+        text: "PVZ",
         href: "https://gilangalrusliadi.github.io/PVZ",
-        imgSrc: "https://static.wikia.nocookie.net/pvz-fusion/images/b/b7/Snipea.png",
-        text: "PVZ"
+        imgSrc: "https://static.wikia.nocookie.net/pvz-fusion/images/b/b7/Snipea.png"
     },
     {
+        text: "Light Novel",
         href: "https://gilangalrusliadi.github.io/LN/",
-        imgSrc: "https://gilangalrusliadi.github.io/LN/images/trapped-in-a-dating-sim-otome-games-are-tough-for-us-too-light-novel-vol-1.jpg",
-        text: "Light Novel"
+        imgSrc: "https://gilangalrusliadi.github.io/LN/images/trapped-in-a-dating-sim-otome-games-are-tough-for-us-too-light-novel-vol-1.jpg"
     },
     {
+        text: "Sparkling Mouse",
         href: "https://gilangalrusliadi.github.io/Sparkling-Cursor/",
-        imgSrc: "https://cdn-icons-png.flaticon.com/256/1890/1890701.png",
-        text: "Sparkling Mouse"
+        imgSrc: "https://cdn-icons-png.flaticon.com/256/1890/1890701.png"
     },
     {
+        text: "Alter Ego",
         href: "https://gilbertclaus.pythonanywhere.com",
-        imgSrc: "https://static.wikia.nocookie.net/tmfanon/images/4/4c/FirstSeraphimCS.png",
-        text: "Alter Ego"
+        imgSrc: "https://static.wikia.nocookie.net/tmfanon/images/4/4c/FirstSeraphimCS.png"
     },
     {
+        text: "Schale Network",
         href: "https://gilbertclaus.pythonanywhere.com/templates/doujin.html",
-        imgSrc: "https://static.wikia.nocookie.net/blue-archive/images/1/12/Schale_Icon_1.png",
-        text: "Schale Network"
+        imgSrc: "https://static.wikia.nocookie.net/blue-archive/images/1/12/Schale_Icon_1.png"
     },
     {
+        text: "Miyuki",
         href: "https://github.com/GilangAlRusliadi/Miyuki",
-        imgSrc: "https://pbs.twimg.com/media/E4032VFXEAglwJ7.jpg",
-        text: "Miyuki"
+        imgSrc: "https://pbs.twimg.com/media/E4032VFXEAglwJ7.jpg"
     }
 ];
 
