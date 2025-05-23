696d647717172d125779f4ee92052fd8fcb63f8e
GilangAlRusliadi <gilangxavier@gmail.com>
2025-05-23 09:45:09 +0700

    Rapi

--- static/markdown.js
+++ static/markdown.js
@@ -1,9 +1,9 @@
 function copyCode(button) {
     const code = button.previousElementSibling.textContent;
     navigator.clipboard.writeText(code).then(() => {
-    button.innerText = 'Copied!';
-    setTimeout(() => {
-        button.innerText = 'Copy';
-    }, 1500);
+        button.innerText = 'Copied!';
+        setTimeout(() => {
+            button.innerText = 'Copy';
+        }, 1500);
     });
 }
